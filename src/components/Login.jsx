f5420c9b109d7fbf24f874a2418e474ebec466e3
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-11-12 03:16:19 +0600

    use useNavigate hooks and go to home page after login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
 
 
 const Login = () => {
 
     const {signIn} = useContext(AuthContext)
+    const navigate = useNavigate()
 
     const handleLogin= e =>{
         e.preventDefault();
@@ -16,6 +17,8 @@ const Login = () => {
         signIn(email,password)
         .then(result => {
             console.log(result.user)
+            e.target.reset();
+            navigate("/")
         })
         .catch(error =>{
             console.log(error)
